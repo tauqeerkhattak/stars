c23f99f269d23a61adb07d5bf9c2a13d0f4f3cfb
tauqeerkhattak <17sw72@students.muet.edu.pk>
2023-01-10 10:57:47 +0500

    obstacles added

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
 class Home extends StatefulWidget {
@@ -17,6 +15,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with TickerProviderStateMixin {
   late AnimationController _slideController;
   late AnimationController _scaleController;
+  late AnimationController _obstacleController;
+  final random = Random();
   final int numberOfStars = kDebugMode ? 50 : 200;
   final _slideDuration = const Duration(
     seconds: 16,
@@ -24,7 +24,11 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   final _scaleDuration = const Duration(
     milliseconds: 2000,
   );
+  final Duration _obstacleDuration = const Duration(
+    seconds: 5,
+  );
   StreamController<Offset> controller = StreamController();
+  double _obstacleYPosition = 0.0;
 
   @override
   void initState() {
@@ -43,6 +47,14 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     )..repeat(
         reverse: true,
       );
+    _obstacleController = AnimationController(
+      vsync: this,
+      duration: _obstacleDuration,
+    )..repeat(
+        reverse: false,
+        min: 0.0,
+        max: 1.0,
+      );
   }
 
   @override
@@ -57,34 +69,38 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
           children: [
             _buildBackground(),
             ..._buildStars(context),
-            StreamBuilder(
-              stream: controller.stream,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Positioned(
-                    left: snapshot.data!.dx,
-                    top: snapshot.data!.dy,
-                    child: Transform.rotate(
-                      angle: -pi / 2,
-                      child: Lottie.asset(
-                        'assets/lottie/rocket.json',
-                        fit: BoxFit.fitWidth,
-                        width: 60,
-                        height: 60,
-                        repeat: true,
-                        reverse: true,
-                      ),
-                    ),
-                  );
-                } else {
-                  return const SizedBox.shrink();
-                }
-              },
-            ),
+            _buildObstacles(),
+            _buildRocket(),
             // _buildNameCard(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildRocket() {
+    return StreamBuilder(
+      stream: controller.stream,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Positioned(
+            left: snapshot.data!.dx,
+            top: snapshot.data!.dy,
+            child: Transform.rotate(
+              angle: -pi / 2,
+              child: Lottie.asset(
+                'assets/lottie/rocket.json',
+                fit: BoxFit.fitWidth,
+                width: 60,
+                height: 60,
+                repeat: true,
+              ),
+            ),
+          );
+        } else {
+          return const SizedBox.shrink();
+        }
+      },
     );
   }
 
@@ -99,7 +115,6 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   List<Widget> _buildStars(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    final random = Random();
     List<Widget> stars = List.generate(
       numberOfStars,
       (index) {
@@ -145,6 +160,38 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     return stars;
   }
 
+  Widget _buildObstacles() {
+    final size = MediaQuery.of(context).size;
+    double top = random.nextInt(size.height.floor()).toDouble();
+    final rockSize = size.width * 0.1;
+    if (top > (size.width - rockSize)) {
+      top -= rockSize;
+    }
+    return AnimatedBuilder(
+      animation: _obstacleController,
+      builder: (context, child) {
+        return Positioned(
+          left: _getObstacleXPosition(
+            width: size.width,
+          ),
+          top: _getObstacleYPosition(
+            height: size.height,
+            width: size.width,
+          ),
+          child: Transform.rotate(
+            angle: 2 * pi * _obstacleController.value,
+            child: child!,
+          ),
+        );
+      },
+      child: Image.asset(
+        'assets/images/rock.png',
+        width: rockSize,
+        height: rockSize,
+      ),
+    );
+  }
+
   double getLeftPosition({
     required double width,
     required double x,
@@ -159,6 +206,36 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       } else {
         return position;
       }
+    }
+  }
+
+  double _getObstacleXPosition({required double width}) {
+    final animationValue = _obstacleController.value;
+    final position = 1.5 * width * animationValue;
+    if (animationValue <= 0.0) {
+      return 0;
+    } else if (animationValue >= width) {
+      return width;
+    } else {
+      return position;
+    }
+  }
+
+  double _getObstacleYPosition(
+      {required double height, required double width}) {
+    final animationValue = _obstacleController.value;
+    final rocketSize = width * 0.1;
+    if (animationValue >= 0.999) {
+      _obstacleYPosition = random.nextInt(height.floor()).toDouble();
+      print(
+          'CHANGE Y POSITION OF OBSTACLE: $_obstacleYPosition ${_obstacleController.duration?.inSeconds}');
+    }
+    if (_obstacleYPosition + rocketSize >= height) {
+      return _obstacleYPosition - (width * 0.1);
+    } else if (_obstacleYPosition + rocketSize <= 0.0) {
+      return _obstacleYPosition + (width * 0.1);
+    } else {
+      return _obstacleYPosition;
     }
   }
 
